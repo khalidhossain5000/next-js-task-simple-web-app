c1b7e00bfeba026d7f9dd01fd9995127b9134888
khalidhossain <mdkhalidhossain5000@gmail.com>
2025-08-21 17:52:24 +0600

    banner responsive added

--- app/Components/HeroSection.jsx
+++ app/Components/HeroSection.jsx
@@ -5,19 +5,19 @@ const HeroSection = () => {
   return (
     <div className="bg-[#EFEBE3]  min-h-screen">
 
-      <div className="container mx-auto flex flex-col justify-center items-center pt-12  lg:pt-16">
+      <div className="container mx-auto flex flex-col md:justify-center items-center pt-6 md:pt-12  lg:pt-16">
         <h2 className="text-xl md:text-2xl lg:text-[56px] text-[#111111] font-semibold">
           Freshness <span className="text-[#179800]">You Can</span>
         </h2>
         <h2 className="text-xl md:text-2xl lg:text-[56px] text-[#111111] font-semibold">
           <span className="text-[#179800]">Count </span>On,Prices You'll Love!
         </h2>
-        <p className="lg:text-xl text-[#111111]">
+        <p className="text-sm px-2 md:px-0 pt-2 md:pt-0 lg:text-xl text-[#111111] text-center">
           Shop your daily essentials at unbeatable prices. From fresh produce to
           pantry staples, we’ve got you covered every day!
         </p>
         <div>
-          <Image src={heroImg} className="w-[80%] mx-auto h-full object-contain"></Image>
+          <Image src={heroImg} className="w-[100%] md:w-[80%] mx-auto h-full md:object-contain"></Image>
         </div>
       </div>
 
